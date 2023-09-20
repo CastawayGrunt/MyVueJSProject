361ba8435e7372fdef80aa9bb5d0ed0258d118aa
Brendon Woodall <brendonwoodall@Brendons-MacBook-Pro.local>
2023-09-19 18:34:06 -0600

    update dashboard

--- TheShelf/src/views/DashboardView.vue
+++ TheShelf/src/views/DashboardView.vue
@@ -12,7 +12,9 @@
               <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
                 Games Owned
               </div>
-              <div class="h5 mb-0 font-weight-bold text-gray-800">12</div>
+              <div class="h5 mb-0 font-weight-bold text-gray-800">
+                {{ gamesSummary.gamesOwned }}
+              </div>
             </div>
             <div class="col-auto">
               <i class="fas fa-chess-pawn fa-2x text-gray-300"></i>
@@ -31,10 +33,12 @@
               <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
                 Last Played Game
               </div>
-              <div class="h5 mb-0 font-weight-bold text-gray-800">Gloomhaven</div>
+              <div class="h5 mb-0 font-weight-bold text-gray-800">
+                {{ gamesSummary.lastPlayed() }}
+              </div>
             </div>
             <div class="col-auto">
-              <img src="@/assets/img/undraw_male_avatar.svg" height="50" />
+              <i class="fas fa-chess-board fa-2x text-gray-300"></i>
             </div>
           </div>
         </div>
@@ -52,12 +56,17 @@
               </div>
               <div class="row no-gutters align-items-center">
                 <div class="col-auto">
-                  <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">Dominion</div>
+                  <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">
+                    {{ gamesSummary.mostPlayedGame().mostPlayedGameName }}
+                    {{ gamesSummary.mostPlayedGame().mostPlayedGamePlays }}
+                  </div>
                 </div>
               </div>
             </div>
             <div class="col-auto d-flex justify-content-center">
-              <div class="text-lg text-gray-700 d-flex justify-content-center">10</div>
+              <div class="text-lg text-gray-700 d-flex justify-content-center">
+                <i class="fas fa-trophy fa-2x text-gray-300"></i>
+              </div>
             </div>
           </div>
         </div>
@@ -71,12 +80,14 @@
           <div class="row no-gutters align-items-center">
             <div class="col mr-2">
               <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                Pending Requests
+                Games Played
               </div>
-              <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
+              <div class="h5 mb-0 font-weight-bold text-gray-800">
+                {{ gamesSummary.gamesPlayed }}
+              </div>
             </div>
             <div class="col-auto">
-              <i class="fas fa-comments fa-2x text-gray-300"></i>
+              <i class="fas fa-dice-six fa-2x text-gray-300"></i>
             </div>
           </div>
         </div>
@@ -84,204 +95,51 @@
     </div>
   </div>
 
-  <!-- Content Row -->
-  <div class="row">
-    <!-- Content Column -->
-    <div class="col-lg-6 mb-4">
-      <!-- Project Card Example -->
-      <div class="card shadow mb-4">
-        <div class="card-header py-3">
-          <h6 class="m-0 font-weight-bold text-primary">Projects</h6>
-        </div>
-        <div class="card-body">
-          <h4 class="small font-weight-bold">
-            Server Migration <span class="float-right">20%</span>
-          </h4>
-          <div class="progress mb-4">
-            <div
-              class="progress-bar bg-danger"
-              role="progressbar"
-              style="width: 20%"
-              aria-valuenow="20"
-              aria-valuemin="0"
-              aria-valuemax="100"
-            ></div>
-          </div>
-          <h4 class="small font-weight-bold">
-            Sales Tracking <span class="float-right">40%</span>
-          </h4>
-          <div class="progress mb-4">
-            <div
-              class="progress-bar bg-warning"
-              role="progressbar"
-              style="width: 40%"
-              aria-valuenow="40"
-              aria-valuemin="0"
-              aria-valuemax="100"
-            ></div>
-          </div>
-          <h4 class="small font-weight-bold">
-            Customer Database <span class="float-right">60%</span>
-          </h4>
-          <div class="progress mb-4">
-            <div
-              class="progress-bar"
-              role="progressbar"
-              style="width: 60%"
-              aria-valuenow="60"
-              aria-valuemin="0"
-              aria-valuemax="100"
-            ></div>
-          </div>
-          <h4 class="small font-weight-bold">
-            Payout Details <span class="float-right">80%</span>
-          </h4>
-          <div class="progress mb-4">
-            <div
-              class="progress-bar bg-info"
-              role="progressbar"
-              style="width: 80%"
-              aria-valuenow="80"
-              aria-valuemin="0"
-              aria-valuemax="100"
-            ></div>
-          </div>
-          <h4 class="small font-weight-bold">
-            Account Setup <span class="float-right">Complete!</span>
-          </h4>
-          <div class="progress">
-            <div
-              class="progress-bar bg-success"
-              role="progressbar"
-              style="width: 100%"
-              aria-valuenow="100"
-              aria-valuemin="0"
-              aria-valuemax="100"
-            ></div>
-          </div>
-        </div>
-      </div>
-
-      <!-- Color System -->
-      <div class="row">
-        <div class="col-lg-6 mb-4">
-          <div class="card bg-primary text-white shadow">
-            <div class="card-body">
-              Primary
-              <div class="text-white-50 small">#4e73df</div>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-6 mb-4">
-          <div class="card bg-success text-white shadow">
-            <div class="card-body">
-              Success
-              <div class="text-white-50 small">#1cc88a</div>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-6 mb-4">
-          <div class="card bg-info text-white shadow">
-            <div class="card-body">
-              Info
-              <div class="text-white-50 small">#36b9cc</div>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-6 mb-4">
-          <div class="card bg-warning text-white shadow">
-            <div class="card-body">
-              Warning
-              <div class="text-white-50 small">#f6c23e</div>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-6 mb-4">
-          <div class="card bg-danger text-white shadow">
-            <div class="card-body">
-              Danger
-              <div class="text-white-50 small">#e74a3b</div>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-6 mb-4">
-          <div class="card bg-secondary text-white shadow">
-            <div class="card-body">
-              Secondary
-              <div class="text-white-50 small">#858796</div>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-6 mb-4">
-          <div class="card bg-light text-black shadow">
-            <div class="card-body">
-              Light
-              <div class="text-black-50 small">#f8f9fc</div>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-6 mb-4">
-          <div class="card bg-dark text-white shadow">
-            <div class="card-body">
-              Dark
-              <div class="text-white-50 small">#5a5c69</div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="col-lg-6 mb-4">
-      <!-- Illustrations -->
-      <div class="card shadow mb-4">
-        <div class="card-header py-3">
-          <h6 class="m-0 font-weight-bold text-primary">Illustrations</h6>
-        </div>
-        <div class="card-body">
-          <div class="text-center">
-            <img
-              class="img-fluid px-3 px-sm-4 mt-3 mb-4"
-              style="width: 25rem"
-              src="img/undraw_posting_photo.svg"
-              alt="..."
-            />
-          </div>
-          <p>
-            Add some quality, svg illustrations to your project courtesy of
-            <a target="_blank" rel="nofollow" href="https://undraw.co/">unDraw</a>, a constantly
-            updated collection of beautiful svg images that you can use completely free and without
-            attribution!
-          </p>
-          <a target="_blank" rel="nofollow" href="https://undraw.co/"
-            >Browse Illustrations on unDraw &rarr;</a
-          >
-        </div>
-      </div>
-
-      <!-- Approach -->
-      <div class="card shadow mb-4">
-        <div class="card-header py-3">
-          <h6 class="m-0 font-weight-bold text-primary">Development Approach</h6>
-        </div>
-        <div class="card-body">
-          <p>
-            SB Admin 2 makes extensive use of Bootstrap 4 utility classes in order to reduce CSS
-            bloat and poor page performance. Custom CSS classes are used to create custom components
-            and custom utility classes.
-          </p>
-          <p class="mb-0">
-            Before working with this theme, you should become familiar with the Bootstrap framework,
-            especially the utility classes.
-          </p>
-        </div>
-      </div>
-    </div>
-  </div>
   <!-- End of Main Content -->
 </template>
 
 <script lang="ts" setup>
 import AppHeader from '@/components/layout/AppHeader.vue'
+import { useUserStore } from '@/stores/user'
+import { computed } from 'vue'
+
+const user = useUserStore().user
+
+const gamesSummary = computed(() => {
+  const gamesOwned = user?.games.length
+
+  const gamesPlayed = user?.games.filter((game) => game.plays.length > 0).length
+
+  const mostPlayedGame = () => {
+    const mostPlayedGame = user?.games
+      .sort((a, b) => b.plays.length - a.plays.length)
+      .slice(0, 1)[0]
+
+    let mostPlayedGameName = ''
+    if (mostPlayedGame?.plays.length === 0) {
+      mostPlayedGameName = 'No games played yet'
+    }
+    let mostPlayedGamePlays = mostPlayedGame?.plays.length
+    if (!mostPlayedGamePlays) {
+      mostPlayedGamePlays = undefined
+    }
+    return { mostPlayedGameName, mostPlayedGamePlays }
+  }
+
+  const lastPlayed = () => {
+    if (user?.lastPlayed) {
+      return user?.lastPlayed
+    } else {
+      return 'No games played yet'
+    }
+  }
+  return {
+    gamesOwned,
+    gamesPlayed,
+    lastPlayed,
+    mostPlayedGame
+  }
+})
 </script>
 
 <style scoped></style>
