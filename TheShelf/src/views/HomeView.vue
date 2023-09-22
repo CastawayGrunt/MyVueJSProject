954133cc624b565323de4e2ab912f1734133e412
Brendon Woodall <brendonwoodall@Brendons-MacBook-Pro.local>
2023-09-21 19:36:41 -0600

    fix css for mobile

--- TheShelf/src/views/HomeView.vue
+++ TheShelf/src/views/HomeView.vue
@@ -1,19 +1,20 @@
 <template>
   <!-- Page Wrapper -->
-  <div id="wrapper" class="d-flex flex-column h-100">
+  <div id="wrapper" class="d-flex flex-column align-items-center h-100">
     <AppHomeNav />
-    <div
-      class="align-items-center justify-content-center flex-column flex-md-row d-flex h-100 gap-5"
-    >
-      <div class="text-white p-4 w-25">
+    <div class="container row my-atuo h-100">
+      <div
+        id="description"
+        class="text-white p-4 col-12 col-md-6 d-flex justify-content-end justify-content-md-center align-items-start flex-column"
+      >
         <h1 class="text-uppercase">The Shelf</h1>
         <p class="text-wrap">
           This is the place to keep track of your game collection. Track the games you own, your
           personal ratings, plays and more!
         </p>
       </div>
-      <div class="">
-        <img height="300" src="@/assets/img/undraw_playing_cards.svg" alt="Playing cards image" />
+      <div class="col-12 col-md-6 d-flex justify-content-center align-items-center">
+        <img width="300" src="@/assets/img/undraw_playing_cards.svg" alt="Playing cards image" />
       </div>
     </div>
   </div>
@@ -22,3 +23,12 @@
 <script setup lang="ts">
 import AppHomeNav from '../components/layout/AppHomeNav.vue'
 </script>
+
+<style scoped>
+#description {
+  width: 100%;
+  @media screen and (min-width: 768px) {
+    width: 100%;
+  }
+}
+</style>
